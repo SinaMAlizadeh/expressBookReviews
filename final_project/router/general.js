e52f19d4970ac32ec54533f0f114b2ef3d94c794
Sina mohammad alizadeh saeed <Sina@Zahras-MacBook-Air.local>
2023-11-26 22:53:46 +0330

    FIX: bugs

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,12 +32,6 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
-  // const isbn = req.params.isbn;
-  // const book = books[isbn];
-  // if (book) res.send(books[isbn]);
-  // else {
-  //   return res.status(404).json({ message: "Book not found" });
-  // }
   let getBookByIsbn = new Promise((resolve, reject) => {
     const isbn = req.params.isbn;
     const book = books[isbn];
@@ -55,14 +49,6 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  // const book = Object.values(books).find(
-  //   ({ author }) => author === req.params.author
-  // );
-  // if (book) res.send(book);
-  // else {
-  //   return res.status(404).json({ message: "Book not found" });
-  // }
   let getBookByAuthor = new Promise((resolve, reject) => {
     const book = Object.values(books).filter(
       ({ author }) => author === req.params.author
@@ -86,14 +72,6 @@ public_users.get("/author/:author", function (req, res) {
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  //Write your code here
-  // const book = Object.values(books).find(
-  //   ({ title }) => title === req.params.title
-  // );
-  // if (book) res.send(book);
-  // else {
-  //   return res.status(404).json({ message: "Book not found" });
-  // }
   let getBookByTitle = new Promise((resolve, reject) => {
     const book = Object.values(books).filter(
       ({ title }) => title === req.params.title
